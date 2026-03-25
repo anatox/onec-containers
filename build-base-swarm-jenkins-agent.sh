2175296234f2f3e1b5d9e8a3d52f676631ef9927
Dmitry Ovcharenko <DOvcharenko@korusconsulting.ru>
2026-03-25 20:08:58 +0300

    sync JDK version across images

--- build-base-swarm-jenkins-agent.sh
+++ build-base-swarm-jenkins-agent.sh
@@ -87,6 +87,7 @@ docker build \
     --build-arg DOCKER_REGISTRY_URL=$DOCKER_REGISTRY_URL \
     --build-arg BASE_IMAGE=onec-client-vnc-oscript \
     --build-arg BASE_TAG=$ONEC_VERSION \
+    --build-arg OPENJDK_VERSION=$OPENJDK_VERSION \
     -t ${DOCKER_REGISTRY_URL:+"$DOCKER_REGISTRY_URL/"}onec-client-vnc-oscript-jdk:$ONEC_VERSION \
     -f jdk/Dockerfile \
     $last_arg
